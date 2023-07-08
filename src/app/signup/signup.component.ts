4d1fc194fa84a1d41c2d17ad9b2341bebf773601
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-07-08 20:19:56 +0200

    quest 05

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  model: User = new User('', '', '', '');
 
+  constructor() {}
+
+  onSubmit(): void {
+    // Formulaire envoyé
+    console.log(this.model);
+  }
 }
